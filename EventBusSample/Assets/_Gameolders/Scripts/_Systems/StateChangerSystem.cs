460fa64dfa9adecfa3f1d6a5166a7c82ae06afaf
berkterek <42406263+berkterek@users.noreply.github.com>
2024-07-28 13:16:41 +0300

    refactor: state changer process completed

--- EventBusSample/Assets/_Gameolders/Scripts/_Systems/StateChangerSystem.cs
+++ EventBusSample/Assets/_Gameolders/Scripts/_Systems/StateChangerSystem.cs
@@ -1,5 +1,6 @@
 using EventBusSample.Components;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace EventBusSample.Systems
@@ -23,8 +24,64 @@ namespace EventBusSample.Systems
             if (!stateListenerData.IsStateChanged) return;
 
             stateListenerData.IsStateChanged = false;
+
+            var entityCommandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+            if (stateListenerData.StateValue)
+            {
+                var job = new PlayStateJob()
+                {
+                    Ecb = entityCommandBuffer.AsParallelWriter()
+                };
+
+                var jobHandle = job.ScheduleParallel(state.Dependency);
+                state.Dependency = jobHandle;
+                jobHandle.Complete();
+            }
+            else
+            {
+                var job = new StopStateJob()
+                {
+                    Ecb = entityCommandBuffer.AsParallelWriter()
+                };
+                
+                var jobHandle = job.ScheduleParallel(state.Dependency);
+                state.Dependency = jobHandle;
+                jobHandle.Complete();
+            }
             
             SystemAPI.SetSingleton(stateListenerData);
+            
+            entityCommandBuffer.Playback(state.EntityManager);
+            entityCommandBuffer.Dispose();
+        }
+
+        [BurstCompile]
+        [WithDisabled(typeof(ActiveTag))]
+        private partial struct PlayStateJob : IJobEntity
+        {
+            public EntityCommandBuffer.ParallelWriter Ecb;
+            
+            [BurstCompile]
+            private void Execute(Entity entity, in DeactivateTag deactivateTag, [ChunkIndexInQuery]int sortKey)
+            {
+                Ecb.SetComponentEnabled<DeactivateTag>(sortKey, entity, false);
+                Ecb.SetComponentEnabled<ActiveTag>(sortKey, entity, true);
+            }
+        }
+        
+        [BurstCompile]
+        [WithDisabled(typeof(DeactivateTag))]
+        private partial struct StopStateJob : IJobEntity
+        {
+            public EntityCommandBuffer.ParallelWriter Ecb;
+            
+            [BurstCompile]
+            private void Execute(Entity entity, in ActiveTag activateTag, [ChunkIndexInQuery]int sortKey)
+            {
+                Ecb.SetComponentEnabled<DeactivateTag>(sortKey, entity, true);
+                Ecb.SetComponentEnabled<ActiveTag>(sortKey, entity, false);
+            }
         }
     }
 }
